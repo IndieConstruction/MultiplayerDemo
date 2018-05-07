fe3d1c57fe3a5fc82a35eab18a299a4ed0d7d2ef
Brago <bragolab@gmail.com>
2018-05-07 18:04:38 +0200

    - Gestion ball server autoritativo.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour {
+
+    public Ball BallPrefab;
+
+    public override void OnStartServer() {
+
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            CmdSpawnBall();
+        }
+    }
+
+    [Command]
+    private void CmdSpawnBall() {
+        NetworkServer.Spawn(Instantiate(BallPrefab.gameObject));
+    }
+}
